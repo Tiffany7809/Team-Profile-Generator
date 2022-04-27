7f6df210752871254081bc1662cdf410a37ff1ae
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-04-26 22:23:56 -0700

    adding basic outline to Main js file

--- Team.js
+++ Team.js
@@ -1,6 +1,37 @@
 //This will hold the code for the Team as a whole
 
-//add inquirer
+//Adding Inquirer
+const inquirer = require("inquirer");
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'Who is the Manager of this Team?',
+      name: 'Manager',
+    },
+    {
+      type: 'input',
+      message: 'Please enter the Managers e-mail.',
+      name: 'E-mail',
+    },
+    {
+      type: 'list',
+      message: 'Would you like to add more to the Team?',
+      name: 'continue?',
+      choices: ['Intern', 'Engineer', 'Manager', 'No Im Done'],
+    },
+  ])
+  .then(function (answers) {
+      console.log(answers)
+  }
+  
+     
+  );
+
+
+
 
 // set up a HTML page that is generated with input information
+const TeamPage = ``
 
